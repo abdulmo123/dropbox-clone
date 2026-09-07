61cbbdffea1543413122cd907ec729b17be1b21a
abdullahi mohamed <abdullahi.mohamed2324@gmail.com>
2026-09-07 14:43:29 -0500

    rename packet

---
+++ dbc-api/src/main/java/com/dbc_api/DbcApiApplication.java
@@ -0,0 +1,13 @@
+package com.dbc_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbcApiApplication.class, args);
+	}
+
+}
