1220a3acd2315943e80276c2f5a995ee3b2e9d3c
abdullahi mohamed <abdullahi.mohamed2324@gmail.com>
2026-09-24 07:43:33 -0500

    refine file upload process

--- dbc-app/src/main/java/com/dbc_api/repository/FileUploadRepository.java
+++ dbc-app/src/main/java/com/dbc_api/repository/FileUploadRepository.java
@@ -1,5 +1,6 @@
 package com.dbc_api.repository;
 
+import com.dbc_api.dto.FileDto;
 import com.dbc_api.model.entity.FileUploadEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -8,16 +9,31 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FileUploadRepository extends JpaRepository<FileUploadEntity, Long> {
+public interface FileUploadRepository extends JpaRepository<FileUploadEntity, String> {
 
-    // query to insert data
-    @Modifying
-    @Query(value = """
-            INSERT INTO dbc.file_upload (id, file_name, file_size, file_location, upload_timestamp)
-            VALUES (:id, :fileName, :fileSize, :fileLocation, CURRENT_TIMESTAMP)
-            """, nativeQuery = true)
-    void saveUploadFileInfo(@Param("id") String id,
+        // query to insert data
+        @Modifying
+        @Query(value = """
+                        INSERT INTO dbc.file_upload (id, file_name, file_size, file_location, upload_timestamp)
+                        VALUES (:id, :fileName, :fileSize, :fileLocation, CURRENT_TIMESTAMP)
+                        """, nativeQuery = true)
+        void saveUploadFileInfo(@Param("id") String id,
                         @Param("fileName") String fileName,
                         @Param("fileSize") Long fileSize,
                         @Param("fileLocation") String fileLocation);
+
+        @Query(value = """
+                        select fu.id, fu.file_name
+                        from dbc.file_upload fu
+                        where fu.file_name = :fileName
+                        """, nativeQuery = true)
+        FileDto checkFileExists(@Param("fileName") String filename);
+
+        @Modifying
+        @Query(value = """
+                        update dbc.file_upload
+                        set upload_timestamp = CURRENT_TIMESTAMP
+                        where file_name = :fileName
+                        """, nativeQuery = true)
+        void updateUploadFileInfo(@Param("fileName") String fileName);
 }
--- dbc-app/src/main/java/com/dbc_api/service/FileUploadServiceImpl.java
+++ dbc-app/src/main/java/com/dbc_api/service/FileUploadServiceImpl.java
@@ -1,9 +1,14 @@
 package com.dbc_api.service;
 
+import com.dbc_api.dto.FileDto;
 import com.dbc_api.model.FileUploadRequest;
 import com.dbc_api.model.FileUploadResponse;
 import com.dbc_api.repository.FileUploadRepository;
 import lombok.extern.slf4j.Slf4j;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,14 +21,19 @@ import java.util.UUID;
 @Slf4j
 public class FileUploadServiceImpl implements FileUploadService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(FileUploadServiceImpl.class);
+
+    @Value("${spring.cloud.azure.storage.container.name}")
+    private String azureContainerName;
+
     private final FileUploadRepository fileUploadRepository;
     private final AzureBlobStorageService azureBlobStorageService;
 
-    public FileUploadServiceImpl (FileUploadRepository fileUploadRepository, AzureBlobStorageService azureBlobStorageService) {
+    public FileUploadServiceImpl(FileUploadRepository fileUploadRepository,
+            AzureBlobStorageService azureBlobStorageService) {
         this.fileUploadRepository = fileUploadRepository;
         this.azureBlobStorageService = azureBlobStorageService;
     }
-
 
     @Transactional
     @Override
@@ -31,17 +41,25 @@ public class FileUploadServiceImpl implements FileUploadService {
         MultipartFile file = fileUploadRequest.file();
         String id = UUID.randomUUID().toString();
 
-
         // push it to Azure Blob storage ?
         azureBlobStorageService.uploadFile(file);
 
-        // save the file info to DB
-        fileUploadRepository.saveUploadFileInfo(
-                id,
-                file.getOriginalFilename(),
-                file.getSize(),
-                ""
-        );
+        FileDto fileDto = new FileDto();
+        fileDto = fileUploadRepository.checkFileExists(file.getOriginalFilename());
+
+        if (fileDto != null) {
+            // update the file info in DB
+            LOGGER.info("File {} already exists...", fileDto.getFileName());
+            fileUploadRepository.updateUploadFileInfo(fileDto.getFileName());
+        } else {
+            // save the file info to DB
+            LOGGER.info("Uploading new file: {}", file.getOriginalFilename());
+            fileUploadRepository.saveUploadFileInfo(
+                    id,
+                    file.getOriginalFilename(),
+                    file.getSize(),
+                    azureContainerName + "/" + file.getOriginalFilename());
+        }
 
         return new FileUploadResponse(id, file.getOriginalFilename(), file.getSize(), "", timestamp);
     }
